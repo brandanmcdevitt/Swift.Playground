e2da31b12e1a55524a9ea9d6166ac8fff24f190f
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2018-07-17 15:40:42 +0100

    Add Day 6

---
+++ HackerRank/30 Days of Code/Day.6.swift
@@ -0,0 +1,33 @@
+//Day 6: Let's Review
+
+import Foundation
+import Darwin
+
+let numStrings = Int(readLine()!)!
+var even = [Character]()
+var odd = [Character]()
+
+func printEvenAndOdd(string: String) {
+    // This prints inputString to stderr for debugging:
+    fputs("string: " + string + "\n", stderr)
+    
+    let input = Array(string)
+    
+    for i in 0..<input.count {
+        if input.index(after: i-1) % 2 == 0 {
+            even.append(input[i])
+        } else if input.index(after: i-1) % 2 != 0 {
+            odd.append(input[i])
+        }
+    }
+    
+    print(String(even) + " " + String(odd))
+    even.removeAll()
+    odd.removeAll()
+}
+
+for _ in 1...numStrings {
+    let inputString = readLine()!
+    printEvenAndOdd(string: inputString)
+}
+
