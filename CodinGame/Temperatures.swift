d1f7c97bb495acb56c4311f0846c6fc54a0c7b07
Brandan McDevitt <brandan.mcdevitt@gmail.com>
2018-09-24 21:19:02 +0100

    Update Temperatures.swift with 100% score

--- CodinGame/Temperatures.swift
+++ CodinGame/Temperatures.swift
@@ -13,14 +13,22 @@ let n = Int(readLine()!)! // the number of temperatures to analyse
 //if there are no temperatures print zero
 if n == 0 {
     print(0)
+} else if n == 1 {
+    let i = Int(readLine()!)!
+    
+    if i == pos {
+        print(pos)
+    } else if abs(i) == neg {
+        print("-\(neg)")
+    }
 } else {
     for i in ((readLine()!).characters.split{$0 == " "}.map(String.init)) {
         let t = Int(i)!
         //if t is less than 0 and absolute t is less than absolute neg
-        if t < 0 && abs(t) < abs(neg) {
+        if t < 0 && abs(t) <= abs(neg) {
             neg = t
         //else if absolute t is less than pos
-        } else if abs(t) < pos {
+        } else if abs(t) <= pos {
             pos = abs(t)
         } 
     }
