b90c0f0f1b6eaed6bc9e61818cc1d157c7441de7
brandanmcdevitt <brandan.mcdevitt@gmail.com>
2019-07-14 11:48:25 +0100

    add project 1

---
+++ Learn/Hacking_with_Swift/Project01/Project01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project01
+//
+//  Created by Brandan McDevitt on 14/07/2019.
+//  Copyright © 2019 Brandan McDevitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
