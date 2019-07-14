26c7b863a1a435d3a00d1b32b32d02d6a7ee71bb
brandanmcdevitt <brandan.mcdevitt@gmail.com>
2019-07-14 19:08:14 +0100

    finish project 1

---
+++ Learn/Hacking_with_Swift/Project01/Project01/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Project01
+//
+//  Created by Brandan McDevitt on 14/07/2019.
+//  Copyright © 2019 Brandan McDevitt. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+}
