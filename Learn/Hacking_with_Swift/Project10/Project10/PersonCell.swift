c8c3e58b9d4f2277be42d7396375ef589dc6b458
brandanmcdevitt <brandan.mcdevitt@gmail.com>
2019-08-06 21:28:28 +0100

    add project 10

---
+++ Learn/Hacking_with_Swift/Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Brandan McDevitt on 06/08/2019.
+//  Copyright © 2019 Brandan McDevitt. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
