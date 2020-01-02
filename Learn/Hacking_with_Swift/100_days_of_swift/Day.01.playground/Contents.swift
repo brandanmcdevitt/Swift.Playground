74d74ade10edd31012f096b4812130cc019a69e1
brandanmcdevitt <brandan.mcdevitt@gmail.com>
2020-01-02 22:49:00 +0000

    Day 01 of 100 days of swift

---
+++ Learn/Hacking_with_Swift/100_days_of_swift/Day.01.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+//strings
+var str = "Hello, playground"
+//ints
+var age = 27
+//underline makes large numbers more readable
+var lotto = 8_000_000
+//multi line strings with \n
+var multiLineString1 = """
+this goes
+over multiple
+lines
+"""
+//multi line strings without \n
+var multiLineString2 = """
+this goes \
+over multiple \
+lines
+"""
+//doubles
+var pi = 3.141
+//booleans
+var awesome = true
+//int score
+var score = 85
+//string interpolation
+var yourScore = "Your score was \(score)"
+var results = "The test results are here: \(yourScore)"
+//constants
+let taylor = "swift"
+//type annotations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
